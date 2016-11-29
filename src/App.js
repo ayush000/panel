2d3b040b7517a4aa626123f083d89bfd8cacbd6d
Ayush Sachdeva <ayush000@gmail.com>
2016-11-30 00:56:50 +0530

    No way to bring them back

--- src/App.js
+++ src/App.js
@@ -1,50 +1,19 @@
 import React, { Component } from 'react';
-// import "../node_modules/react-vis/main.css";
+import update from 'immutability-helper';
 import _ from 'lodash'
-// import { Table } from 'react-vis';
-import { CartesianGrid, ResponsiveContainer, LineChart, Line, XAxis, YAxis, Tooltip, Legend } from 'recharts';
+import { Brush, CartesianGrid, ResponsiveContainer, LineChart, Line, XAxis, YAxis, Tooltip, Legend } from 'recharts';
 
 import logo from './logo.svg';
 import './App.css';
 
-// const data = [
-//   { name: '2016-01-01', uv: 1000, pv: 2400, amt: 2400 },
-//   { name: '2016-01-02', uv: 300, pv: 4567, amt: 2400 },
-//   { name: '2016-01-03', uv: 280, pv: 1398, amt: 2400 },
-//   { name: '2016-01-04', uv: 200, pv: 9800, amt: 2400 },
-//   { name: '2016-01-05', uv: 278, pv: 3908, amt: 2400 },
-//   { name: '2016-01-06', uv: 189, pv: 4800, amt: 2400 },
-//   { name: '2016-01-07', uv: 189, pv: 4800, amt: 2400 },
-//   { name: '2016-01-08', uv: 189, pv: 4800, amt: 2400 },
-//   { name: '2016-01-09', uv: 189, pv: 4800, amt: 2400 },
-//   { name: '2016-01-10', uv: 189, pv: 4800, amt: 2400 },
-// ];
-
-// const baseUrl = 'https://prod-analytics-in.jugnoo.in:8083';
-const baseUrl = 'http://localhost:8081';
+const baseUrl = 'https://prod-analytics-in.jugnoo.in:8083';
+// const baseUrl = 'http://localhost:8081';
 const colors = ["#023fa5", "#7d87b9", "#bec1d4",
   "#d6bcc0", "#bb7784", "#8e063b", "#4a6fe3",
   "#8595e1", "#b5bbe3", "#e6afb9", "#e07b91",
   "#d33f6a", "#11c638", "#8dd593", "#c6dec7",
   "#ead3c6", "#f0b98d", "#ef9708", "#0fcfc0",
   "#9cded6", "#d5eae7", "#f3e1eb", "#f6c4e1", "#f79cd4"];
-// class LineGroup extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       data: null,
-//     };
-//   }
-//   render() {
-//     console.log(this.props.columns);
-
-//     return (
-//       <div>
-
-//       </div>
-//     );
-//   }
-// }
 
 class Graph extends Component {
   constructor() {
@@ -52,6 +21,7 @@ class Graph extends Component {
     this.state = {
       rows: [],
       columns: [],
+      enabled: {},
     }
   }
 
@@ -77,24 +47,32 @@ class Graph extends Component {
       .then(parseJSON)
       .then(data => {
         this.setState({ rows: data.result });
-        this.setState({ columns: _.without(Object.keys(this.state.rows[0]), ['timing', 'columnsCount']) });
+        this.setState({ columns: Object.keys(this.state.rows[0]).slice(2) });
+        // store true for each legend
+        const legendsEnabled = this.state.columns.reduce((obj, elem) => {
+          obj[elem] = true;
+          return obj;
+        }, {});
+        // nest legend within qkey
+        const enabled = { [this.props.qkey]: legendsEnabled };
+        this.setState({ enabled: enabled });
+        console.log(this.state.enabled);
       })
       .catch(error => {
         throw error;
       });
   }
-  // render() {
-  //   return (<Table
-  //     height={300}
-  //     width={400}
-  //     data={[
-  //       [1, 2, 3, 4, 5],
-  //       ['one', 'two', 'three', 'four', 'five']
-  //     ]}
-  //     header={
-  //       ['h1', 'h2', 'h3', 'h4', 'h5']
-  //     } />);
-  // }
+  handleClick(i) {
+    console.log(i);
+    console.log(this.state.enabled[this.props.qkey]);
+    this.setState({
+      // Set enabled = false for legend clicked
+      enabled: update(this.state.enabled, {
+        [this.props.qkey]: { [i.dataKey]: { $set: !this.state.enabled[this.props.qkey][i.dataKey] } }
+      })
+    });
+    console.log(this.state.enabled[this.props.qkey]);
+  }
   render() {
     if (this.state.columns.length > 0) {
       return (
@@ -105,13 +83,26 @@ class Graph extends Component {
               <XAxis dataKey="timing" />
               <YAxis label={this.props.data.yAxisTitle} />
               <CartesianGrid strokeDasharray="3 3" />
-              <Tooltip cursor={false}/>
-              <Legend />
-              {this.props.data.columnNames.map((col, i) =>
-                <Line key={col} type="monotone"
-                  dot={false}
-                  dataKey={this.state.columns[i]}
-                  name={col} stroke={colors[i % colors.length]} />)}
+              <Tooltip cursor={false} />
+              <Legend onClick={i => {
+                this.handleClick(i);
+              } } verticalAlign="top" height={36} />
+              {this.props.data.columnNames
+                .filter((col, i) => {
+                  // If qkey exists and legend for qkey is enabled
+                  if (this.state.enabled[this.props.qkey] &&
+                    this.state.enabled[this.props.qkey][this.state.columns[i]]) {
+                    return true;
+                  }
+                  return false;
+                })
+                .map((col, i) =>
+                  // qkey + legend datakey is unique
+                  <Line key={`${this.props.qkey} + ${col}`} type="monotone"
+                    dot={false}
+                    dataKey={this.state.columns[i]}
+                    name={col} stroke={colors[i % colors.length]} />)}
+              <Brush dataKey='timing' height={30} stroke="#8884d8" />
             </LineChart>
           </ResponsiveContainer>
         </div>
@@ -164,7 +155,7 @@ class Panel extends Component {
         {Object.keys(this.state.graphDetails)
           .map((qkey) =>
             <Graph key={qkey} qkey={qkey} data={this.state.graphDetails[qkey]} />
-          )};
+          )}
       </div>);
 
     }
